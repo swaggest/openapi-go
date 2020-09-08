b86f953b37af9d0e3cafe1e5dafc34b88ebe6116
Viacheslav Poturaev <vearutop@gmail.com>
2020-09-08 13:09:14 +0200

    Add RequestBodyEnforcer

--- openapi3/reflect.go
+++ openapi3/reflect.go
@@ -72,6 +72,7 @@ const (
 // RequestBodyEnforcer enables request body for GET and HEAD methods.
 //
 // Should be implemented on input structure, function body can be empty.
+// Forcing request body is not recommended and should only be used for backwards compatibility.
 type RequestBodyEnforcer interface {
 	ForceRequestBody()
 }
