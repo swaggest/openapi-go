54f473fd1d4a0d45e03d655b4fef3dc6c56984ab
Viacheslav Poturaev <vearutop@gmail.com>
2022-11-14 09:57:44 +0100

    Fix validation of http bearer scheme

--- openapi3/entities.go
+++ openapi3/entities.go
@@ -6792,6 +6792,26 @@ type NonBearer struct {
 	Scheme *interface{} `json:"scheme,omitempty"`
 }
 
+// UnmarshalJSON decodes JSON.
+func (b *NonBearer) UnmarshalJSON(data []byte) error {
+	var err error
+
+	var rawMap map[string]json.RawMessage
+
+	err = json.Unmarshal(data, &rawMap)
+	if err != nil {
+		rawMap = nil
+	}
+
+	if v, exists := rawMap["scheme"]; exists && string(v) == `"bearer"` {
+		return fmt.Errorf(`bad const value for "scheme" (not "bearer" expected, %s received)`, v)
+	}
+
+	delete(rawMap, "scheme")
+
+	return nil
+}
+
 // WithScheme sets Scheme value.
 func (n *NonBearer) WithScheme(val interface{}) *NonBearer {
 	n.Scheme = &val
