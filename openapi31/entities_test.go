6f1eaab7a82fce81b789f795631800c700a37096
Viacheslav Poturaev <vearutop@gmail.com>
2023-09-29 10:10:55 +0200

    Fix reference schema to enable mutual exclusiveness

---
+++ openapi31/entities_test.go
@@ -0,0 +1,17 @@
+package openapi31_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/swaggest/openapi-go/openapi31"
+)
+
+func TestSpec_UnmarshalYAML(t *testing.T) {
+	bytes, err := os.ReadFile("_testdata/albums_api.yaml")
+	require.NoError(t, err)
+
+	refl := openapi31.NewReflector()
+	require.NoError(t, refl.Spec.UnmarshalYAML(bytes))
+}
